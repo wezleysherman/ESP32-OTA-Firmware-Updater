385e7d405c141b1af4ff409367f20d63a190c73f
Wezley Sherman <wezleysherman@yahoo.com>
2019-02-18 22:16:31 -0700

    asdgf

--- TrynkitOTAFirmware.h
+++ TrynkitOTAFirmware.h
@@ -4,8 +4,12 @@
 #include <BLEDevice.h>
 #include <BLE2902.h>
 #include <BLEServer.h>
+#include <EEPROM.h>
 #include <Update.h>
+#include "ArduinoJson.h"
 #include "ReceiveCallBack.h"
+#include "esp_ota_ops.h"
+#include <esp_partition.h>
 
 // BLE UUID's for Trynkit Device
 // See the following for generating UUIDs:
@@ -13,12 +17,6 @@
 #define SERVICE_UUID           "0000ffe0-0000-1000-8000-00805f9b34fb" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "80c78362-e3f9-11e8-9f32-f2801f1b9fd1"
 #define CHARACTERISTIC_UUID_TX "80c784ac-e3f9-11e8-9f32-f2801f1b9fd1"
-
-//Partition characteristics
-#define APP0_ADDR 0x10000
-#define APP0_SIZE 0x113E10
-#define APP1_ADDR 0x123E10
-#define APP1_SIZE 0x251430
 
 // Global Variables
 BLEServer *pServer = NULL;
@@ -31,8 +29,9 @@ bool transmit = false;
 String image = ""; // image to flash
 bool oldDeviceConnected = false;
 uint8_t updateCount = 0; //increment for every write and flush of the update
-const uint32_t UPDATE_SIZE = 1000;
+const uint32_t UPDATE_SIZE = 4800;
 const esp_partition_t* PART;
+int flag = 0;
 
 // Method defines
 void initBLE();
