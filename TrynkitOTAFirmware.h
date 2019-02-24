3609f2d909cb54d516c19e03912d4c0d9961b21e
Wezley Sherman <wezleysherman@yahoo.com>
2019-02-24 01:16:33 -0700

    status indicator/stability stuffs

--- TrynkitOTAFirmware.h
+++ TrynkitOTAFirmware.h
@@ -4,9 +4,7 @@
 #include <BLEDevice.h>
 #include <BLE2902.h>
 #include <BLEServer.h>
-#include <EEPROM.h>
 #include <Update.h>
-#include "ArduinoJson.h"
 #include "ReceiveCallBack.h"
 #include "esp_ota_ops.h"
 #include <esp_partition.h>
@@ -22,21 +20,21 @@
 BLEServer *pServer = NULL;
 BLECharacteristic * pTxCharacteristic;
 
+volatile int dir = 0;
+volatile int indexBlink = 0;
 bool deviceConnected = false;
 uint8_t out_buff[7];
 bool receiveImage = false;
 bool transmit = false;
 String image = ""; // image to flash
 bool oldDeviceConnected = false;
-uint8_t updateCount = 0; //increment for every write and flush of the update
 const uint32_t UPDATE_SIZE = 960;
 const esp_partition_t* PART;
-int flag = 0;
+std::vector<uint8_t> fImage;
 
 // Method defines
 void initBLE();
 void deinitBLE();
 void reset();
-void writeFirmware(esp_partition_t* part, uint32_t addr, uint32_t partSize);
 
 #endif
